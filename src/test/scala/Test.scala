eb642fc662865947a9e65a82fa21ed72823523c0
djx314 <djx314@sina.cn>
2018-08-17 19:25:04 +0800

    一些测试。

--- src/test/scala/Test.scala
+++ src/test/scala/Test.scala
@@ -7,15 +7,17 @@ import akka.http.scaladsl.server._
 
 trait Test extends App with AkkaHttpParameterHelper {
 
-  case class RequestModel1(id: String, age: Int, name: String)
+  case class RequestModel1(id: String, age: Int, name: String, account: String, num: Int)
 
   val pro = new DecoderProvenShape[RequestModel1] {
     val id = helper.parameter("id", "id")
     val age = helper.formField("age", "age".as[Int])
     val name = helper.formField("name", "name")
+    val account = helper.param("account")
+    val num = helper.form("num")
 
     override def sv: DecoderShapeValue[RequestModel1] = {
-      fromModel.shaped(id :: age :: name :: HNil)
+      fromModel.shaped(id :: age :: name :: account :: num :: HNil)
     }
   }
 
